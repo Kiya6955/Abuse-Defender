36563be6fd032f4d777921b09b940d69e85895aa
Kiya6955 <102658999+Kiya6955@users.noreply.github.com>
2024-08-17 17:22:12 +0330

    Update abuse-defender.sh

--- abuse-defender.sh
+++ abuse-defender.sh
@@ -16,19 +16,17 @@ function main_menu {
     echo "1-Block Abuse IP-Ranges"
     echo "2-Whitelist an IP/IP-Ranges manually"
     echo "3-Block an IP/IP-Ranges manually"
-    echo "4-Block Torrent"
-    echo "5-View Rules"
-    echo "6-Clear all rules"
-    echo "7-Exit"
+    echo "4-View Rules"
+    echo "5-Clear all rules"
+    echo "6-Exit"
     read -p "Enter your choice: " choice
     case $choice in
     1) block_ips ;;
     2) whitelist_ips ;;
     3) block_custom_ips ;;
-    4) block_torrent ;;
-    5) view_rules ;;
-    6) clear_chain ;;
-    7) echo "Exiting..."; exit 0 ;;
+    4) view_rules ;;
+    5) clear_chain ;;
+    6) echo "Exiting..."; exit 0 ;;
     *) echo "Invalid option"; main_menu ;;
     esac
 }
@@ -115,15 +113,6 @@ function block_custom_ips {
     main_menu
 }
 
-function block_torrent {
-    clear
-    bash -c "$(curl -fsSL https://raw.githubusercontent.com/090ebier/torrent-blocking-script/main/block_torrent.sh)"
-    clear
-    echo "Torrent blocked successfully."
-    read -p "Press enter to return to Menu" dummy
-    main_menu
-}
-
 function view_rules {
     clear
     iptables -L abuse-defender -n
@@ -135,9 +124,6 @@ function clear_chain {
     clear
     iptables -F abuse-defender
     sudo sed -i '/127.0.0.1 appclick.co/d' /etc/hosts
-    iptables -F TORRENT_CHAIN_INPUT
-    iptables -F TORRENT_CHAIN_OUTPUT
-    iptables -F TORRENT_CHAIN_FORWARD
     iptables-save > /etc/iptables/rules.v4
     clear
     echo "All Rules cleared successfully."
